5e43a0e99a0c704863123759a6fe911696d25e79
realChainLife <realchainlife@outlook.com>
2018-05-28 19:29:11 +0300

    initial commit frontend config

---
+++ config/webpack.front.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// const path = require('path');
+const webpack = require('webpack');
+
+const createFrontendLoaders = require('./lib/createFrontendLoaders');
+const entry = require('./lib/entry');
+const output = require('./lib/output');
+const resolve = require('./lib/resolve');
+const includedDirectories = require('./lib/includedDirectories');
+
+module.exports = {
+    entry,
+
+    target: 'web',
+
+    output,
+
+    module: {
+        rules: createFrontendLoaders(includedDirectories),
+    },
+
+    resolve,
+
+    plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+        new webpack.NamedModulesPlugin(),
+    ],
+
+    devtool: '#eval-source-map',
+};
