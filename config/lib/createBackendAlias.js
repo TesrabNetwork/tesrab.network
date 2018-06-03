2151771ca93cb35e7e02e23a03b6a22ce0dd6d4d
realChainLife <realchainlife@outlook.com>
2018-06-03 22:32:09 +0300

    initial backend alias creation

---
+++ config/lib/createBackendAlias.js
@@ -0,0 +1,10 @@
+module.exports = ({ production = false } = {}) => ({})
+  // production
+  //   ? {}
+  //   : {
+  //       'src/common/containers/Main/index': 'src/common/components/Null',
+  //       'src/common/containers/Developers/index': 'src/common/components/Null',
+  //       'src/common/containers/Company/index': 'src/common/components/Null',
+  //       'src/common/containers/Product/index': 'src/common/components/Null',
+  //       'src/common/containers/GetWaves/index': 'src/common/components/Null',
+  //     };
