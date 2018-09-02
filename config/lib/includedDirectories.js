77f47fb2e5d1c6d7f64230ecee38204cf259e3cc
RealChain Life <realchainlife@protonmail.com>
2018-09-02 11:47:53 +0300

    Update includedDirectories.js

--- config/lib/includedDirectories.js
+++ config/lib/includedDirectories.js
@@ -3,5 +3,5 @@ const { include } = require('path');
 module.exports = [
   include(__dirname, '../../src/public'),
   include(__dirname, '../../src/common'),
-  join(__dirname, '../../node_modules/swiper'),
+  include(__dirname, '../../node_modules/swiper'),
 ];
