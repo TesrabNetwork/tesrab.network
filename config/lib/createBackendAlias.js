344194d8e20441fd6a2b8560824cae324f4b9e70
RealChain Life <realchainlife@protonmail.com>
2018-12-07 17:35:01 +0300

    Update createBackendAlias.js

--- config/lib/createBackendAlias.js
+++ config/lib/createBackendAlias.js
@@ -5,6 +5,6 @@ module.exports = ({ production = false } = {}) => ({})
   //       'src/common/containers/Main/index': 'src/common/components/Null',
   //       'src/common/containers/Developers/index': 'src/common/components/Null',
   //       'src/common/containers/Company/index': 'src/common/components/Null',
-  //       'src/common/containers/Product/index': 'src/common/components/Null',
+  //       'src/common/containers/Service/index': 'src/common/components/Null',
   //       'src/common/containers/GetTesraCoin/index': 'src/common/components/Null',
   //     };
